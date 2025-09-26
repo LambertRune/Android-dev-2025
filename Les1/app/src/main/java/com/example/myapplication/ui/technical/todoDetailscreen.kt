834b8769a2bb84e2aefa692ff22c5e4540b7632c
lambertR <rune.lambert@student.vives.be>
2025-09-26 11:29:48 +0200

    added Les2 project

--- Les1/app/src/main/java/com/example/myapplication/ui/technical/todoDetailscreen.kt
+++ Les1/app/src/main/java/com/example/myapplication/ui/technical/todoDetailscreen.kt
@@ -7,11 +7,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.example.myapplication.R
 
 @Composable
 fun toDoDetailScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
-        Text(text = "Detail")
+        Text(
+            text = "Detail",
+            color = colorResource(id = R.color.purple_500)
+        )
         HorizontalDivider(thickness = DividerDefaults.Thickness, color = Color.Green)
     }
 }
---
+++ Les2/app/src/main/java/com/example/les2/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.les2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.les2.ui.theme.Les2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Les2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Les2Theme {
+        Greeting("Android")
+    }
+}
